783847f32f4417cd018dd3c20caf350a008de6e8
htrivino20 <alexander200036@hotmail.com>
2024-06-12 20:18:02 -0500

    feat(variable): integrated openai api key as env variable

--- app/clients/openai.py
+++ app/clients/openai.py
@@ -1,9 +1,9 @@
 from openai import AsyncOpenAI
-
+import os
 class OpenAIModule:
     def __init__(self):
         self._client = AsyncOpenAI(
-            api_key='OPENAI_API_KEY'
+            api_key=os.environ['OPENAI_API_KEY']
         )
 
     async def transcribe_audio(self, path: str):
